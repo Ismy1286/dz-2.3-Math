ea2e7ab1dc26405df43d88e2d6b14e44eab66439
Ainur <ismy1286@gmail.com>
2021-10-22 16:09:06 +0600

    first commit

--- dz-2.3-Math/Math.swift
+++ dz-2.3-Math/Math.swift
@@ -1,8 +1,67 @@
-//
-//  Math.swift
-//  dz-2.3-Math
-//
-//  Created by user on 22.10.2021.
-//
 
 import Foundation
+
+// корень, степени
+
+
+class Math {
+    var result = 0.0
+    
+    func calculator(operation: String, number: Double) -> Double {
+        
+        if operation == "√" {
+            var step = 0.01
+            while result != number / step {
+                if step * step == number {
+                    result = step
+                } else {
+                    step = step + 0.01
+                }
+            }
+        }
+        
+        if operation == "n²" {
+            result = number * number
+        }
+        
+        return result
+    }
+    
+    
+// синуса, косинуса
+    
+    func calculator(operation: String, numberA: Double, numberB: Double, numberC: Double) -> Double {
+        if operation == "sin" {
+            result = numberB / numberC
+        }
+        
+        if operation == "cos" {
+            result = numberA / numberC
+        }
+        
+        return result
+    }
+    
+// плюса, минуса, умножения, деления, процента
+    
+    func calculator(operation: String, numberOne: Double, numberTwo: Double) -> Double {
+        
+        if operation == "+" {
+            result = numberOne + numberTwo
+        }
+        if operation == "-" {
+            result = numberOne - numberTwo
+        }
+        if operation == "*" {
+            result = numberOne * numberTwo
+        }
+        if operation == "/" {
+            result = numberOne / numberTwo
+        }
+        if operation == "%" {
+            result = (numberOne * numberTwo) / 100
+        }
+        return result
+    }
+}
+
--- dz-2.3-Math/main.swift
+++ dz-2.3-Math/main.swift
@@ -1,11 +1,70 @@
-//
-//  main.swift
-//  dz-2.3-Math
-//
-//  Created by user on 22.10.2021.
-//
 
-import Foundation
 
-print("Hello, World!")
+// Проверка  -  корень, степени, синуса, косинуса
+
+var math = Math()
+
+if math.calculator(operation: "√", number: 0.0064) == 0.08 {
+    print("'√' work")
+} else {
+    print("'√' not work")
+}
+
+if math.calculator(operation: "n²", number: 12.23) == 149.5729 {
+    print("'n²' work")
+} else {
+    print("'n²' not work")
+}
+
+if math.calculator(operation: "sin", numberA: 33.0, numberB: 44.0, numberC: 55.0) == 0.8 {
+    print("'sin' work")
+} else {
+    print("'sin' not work")
+}
+
+if math.calculator(operation: "cos", numberA: 33.0, numberB: 44.0, numberC: 55.0) == 0.6 {
+    print("'cos' work")
+} else {
+    print("'cos' not work")
+}
+
+
+// Проверка  -   плюса, минуса, умножения, деления, процента
+
+
+if math.calculator(operation: "+", numberOne: 125.32, numberTwo: 35.65) == 160.97 {
+    print("'+' work")
+} else {
+    print("'+' not work")
+}
+
+if math.calculator(operation: "-", numberOne: 3245.43, numberTwo: 2232.12) == 1013.31 {
+    print("'-' work")
+} else {
+    print("'-' not work")
+}
+
+if math.calculator(operation: "*", numberOne: 125.32, numberTwo: 85.22) == 10679.7704 {
+    print("'*' work")
+} else {
+    print("'*' not work")
+}
+
+if math.calculator(operation: "/", numberOne: 73674.3429, numberTwo: 58.23) == 1265.23 {
+    print("'/' work")
+} else {
+    print("'/' not work")
+}
+
+if math.calculator(operation: "%", numberOne: 458.32, numberTwo: 52.5) == 240.618 {
+    print("'%' work")
+} else {
+    print("'%' not work")
+}
+
+
+
+
+
+
 
